27b8215bbb737cb9932c78c4b840d055e6baebfb
rhovey <robert.hovey95@gmail.com>
2022-01-29 06:39:26 -0800

    first commit with new project and env vars

---
+++ algorithms/models.py
@@ -0,0 +1,23 @@
+import datetime
+from django.db import models
+from django.utils import timezone
+
+class Test(models.Model):
+    text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
+class AlgorithmType(models.Model):
+    algo_type = models.CharField(max_length=200)
+    algo_description = models.TextField(max_length=2000)
+    algo_thumbnail = models.ImageField(upload_to='algo_thumbs/')
+    
+    def __str__(self):
+        return self.algo_type
+    
+    @property
+    def str_to_url(self):
+        return '_'.join(self.algo_type.lower().split(' '))
---
+++ algorithms/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from .models import AlgorithmType
+
+def index(request):
+    latest_algo_list = AlgorithmType.objects.all()
+    context = {
+        'latest_algo_list': latest_algo_list,
+    }
+    return render(request, 'algorithms/index.html', context)
